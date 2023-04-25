306be8bcc8e2e4d8908e7dfb9fcfc182e1e76514
Peter Llerena <76959331+peterllerena17@users.noreply.github.com>
2023-04-25 21:38:03 +0000

    iterating through arrays

--- src/App.java
+++ src/App.java
@@ -1,19 +1,35 @@
+import java.util.Arrays;
+
 public class App {
-  
+
   public static void main(String[] args) {
 
+
+    // creating new arrays
     double[] lotteryNums = new double[5];
-    double[] lotteryNums2 = {1,2,4,5,6};
+    double[] lotteryNums2 = { 1, 2, 4, 5, 6 };
 
     System.out.println(lotteryNums2[2]);
 
+    // assinging new value to index 2
     lotteryNums2[2] = 40;
-
     System.out.println(lotteryNums2[2]);
 
+    //iteration
+    for(int i = 0; i < lotteryNums2.length; i++){
+      System.out.println(lotteryNums2[i]);
+    }
+    System.out.println();
+
+    //for each loop
+    for(double j : lotteryNums2){
+      System.out.println(j);
+    }
+    System.out.println();
 
 
-
+    //using streams
+    Arrays.stream(lotteryNums2).forEach(System.out::println);
 
     
   }
